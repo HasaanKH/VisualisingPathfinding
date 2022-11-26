afab5d9ccdc73f728ae29a7f7f9fe2603a259321
hasKhan-cloud <78858679+hasKhan-cloud@users.noreply.github.com>
2022-11-26 22:28:38 +0000

    wallcreation completed and phase var added to node

--- logic.js
+++ logic.js
@@ -1,9 +1,17 @@
-function createNode (row, column, weight) { //returns node
+function createNode (row, column, heuristic, phase) { //returns node
     var node = document.createElement("div");
     node.setAttribute("row", row);
     node.setAttribute("column", column);
-    node.setAttribute("weight", weight);
+    node.setAttribute("heuristic", heuristic);
+    node.setAttribute("phase", phase) //determines if passable, 1 is true
     node.classList.add("gridElement");
+    node.addEventListener("mouseover", function(){
+        if (this.classList.contains('wallEditing')){
+        this.style.backgroundColor = 'black';
+        this.phase = 0;
+    }
+    }
+    );
     return node;
 }
 
@@ -11,10 +19,9 @@ function createNode (row, column, weight) { //returns node
 function createGrid () { 
     var grid = document.getElementById('Grid');
     var col = []; 
-    var 
     for (x = 1; x <= 25; x ++ ){
         for (y = 1; y<= 10; y++) {
-            var tempNode = createNode (y, x, 1);
+            var tempNode = createNode (y, x, 1, 1);
             grid.appendChild(tempNode);
         }
     }
@@ -22,9 +29,29 @@ function createGrid () {
 
 function setNodes () { 
     const nodeList = document.getElementById('Grid').childNodes;
-    console.log(nodeList[0]);
     nodeList[0].style.backgroundColor = 'red';
     nodeList[249].style.backgroundColor = 'red';
+}
+
+var click = 0;
+
+function wallEdit() {
+    click ++;
+    let children = document.getElementById("Grid").childNodes;
+    for (i = 1; i <= 248; i++) {
+        console.log(children[i]);
+        let tempNode = children[i];
+        if (click % 2 === 1){
+            tempNode.classList.add("wallEditing");
+        }
+        else {
+            tempNode.classList.remove("wallEditing");
+        }
+    }
+}
+
+function refresh() {
+    createGrid();
 }
 
 
