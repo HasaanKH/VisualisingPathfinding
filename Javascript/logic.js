95e144ec81cfe5c57ece1720288b4b77aad6bcf3
hasKhan-cloud <78858679+hasKhan-cloud@users.noreply.github.com>
2022-12-30 01:00:14 +0000

    refactoring nodefactoryfunc

--- Javascript/logic.js
+++ Javascript/logic.js
@@ -1,5 +1,6 @@
 import { MinHeap } from './MinheapClass.js';
 import { dijkstraAlgo } from './DijkstraAlgo.js';
+import { createNode } from './nodeFactoryfunc.js';
 
 export var nodeList;
 export var distances = new Map(); //map of node to distance.
@@ -13,25 +14,6 @@ var gridX = 25;
 window.dijkstraAlgo = dijkstraAlgo; //necessary for onclick events
 window.wallEdit = wallEdit;
 
-
-function createNode (row, column, heuristic, phase) { //returns node
-    var node = document.createElement("div");
-    node.setAttribute("row", row);
-    node.setAttribute("column", column);
-    node.setAttribute("heuristic", heuristic);
-    node.setAttribute("phase", phase) //determines if passable, 1 is true
-    node.setAttribute('sig', 0) //whether the node is significant or not e.g start = 1.
-    node.setAttribute("id", row.toString() +' '+ column.toString());
-    node.classList.add("gridElement");
-    node.addEventListener("mouseover", function(){
-        if (this.classList.contains('wallEditing')){
-        this.style.backgroundColor = 'black';
-        this.phase = 0;
-    }
-    }
-    );
-    return node;
-}
 
 function createGrid () {  //appends x axis wise
     grid = document.getElementById('Grid');
---
+++ Javascript/nodeFactoryfunc.js
@@ -0,0 +1,18 @@
+export function createNode (row, column, heuristic, phase) { //returns node
+    var node = document.createElement("div");
+    node.setAttribute("row", row);
+    node.setAttribute("column", column);
+    node.setAttribute("heuristic", heuristic);
+    node.setAttribute("phase", phase) //determines if passable, 1 is true
+    node.setAttribute('sig', 0) //whether the node is significant or not e.g start = 1.
+    node.setAttribute("id", row.toString() +' '+ column.toString());
+    node.classList.add("gridElement");
+    node.addEventListener("mouseover", function(){
+        if (this.classList.contains('wallEditing')){
+        this.style.backgroundColor = 'black';
+        this.phase = 0;
+    }
+    }
+    );
+    return node;
+}
